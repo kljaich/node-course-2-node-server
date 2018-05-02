5488de4856439c2e7281e100989ef4908e572bba
Joseph Kljaich <kljaich@gmail.com>
2018-05-02 07:29:56 -0500

    Reorder app.use for maintenance.html

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+// If executing on local laptop, port 3000 to be used.  If executing from
+// heroku, then heroku assigns the port the client/requester will use.
 const port = process.env.PORT || 3000;
 
 var app = express();
-console.log(__dirname + '\\public');
 
 hbs.registerPartials(__dirname + '\\views\\partials')
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '\\public'));
 
 app.use((request, response, next) => {
   var now = new Date().toString();
@@ -21,21 +21,16 @@ app.use((request, response, next) => {
     }
   });
 
-  response.render('maintenance.hbs');
-});
-
-app.use((request, response, next) => {
-  var now = new Date().toString();
-  log = `${now}: ${request.method} ${request.url}`;
-  console.log(log);
-  fs.appendFile('server.log', log + '\n', (err) => {
-    if (err) {
-      console.log('Unable to append to server.log')
-    }
-  });
+  // response.render('maintenance.hbs');
   next();
 });
 
+// Order of app.use important, as don't want to support rendering of
+// help.html when website is under maintenance.
+app.use(express.static(__dirname + '\\public'));
+
+// Helpers are preferred before property data.  Helpers are called
+// directly in html files.
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
@@ -44,16 +39,9 @@ hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 })
 
+// root route sets two properties to be passed to dynamic web pages for
+// rendering/display.
 app.get('/', (request, response) => {
-  // response.send('<h1>Hello Express</h1>');
-  // response.send({
-  //   name: 'Jeff',
-  //   likes: [
-  //     'Rock Climbing',
-  //     'Cooking',
-  //     'Running'
-  //   ]
-  // })
   response.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to this website, I hope you enjoy your visit',
